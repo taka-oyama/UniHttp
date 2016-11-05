92443b18f6eefb13d71853af76c8e4de63f2b1c9
Taka Oyama <taka.oyama@gmail.com>
2016-11-05 16:55:00 +0900

    clean up

--- Assets/Example/Test.cs
+++ Assets/Example/Test.cs
@@ -1,29 +1,21 @@
 ﻿using UnityEngine;
 using System;
 using UniHttp;
-using System.Collections.Generic;
 
 public class Test : MonoBehaviour {
 	void Start () {
 		HttpManager.Initalize();
 
 		HttpClient client = new HttpClient();
-
 		var uri = new Uri("http://localhost:3000/test/debug.json");
-		var payload = new HttpJsonData(new TestClass() { level = 10 });
-
-		var request = new HttpRequest(HttpMethod.GET, uri, null, payload);
+		var payload = new HttpJsonData(new TestClass() { level = 10, stat = "!!#^(0-=" });
+		var request = new HttpRequest(HttpMethod.GET, uri, payload);
 		client.Send(request, response => {});
-//
-//		var request2 = new HttpRequest(uri, HttpMethod.GET, null, payload);
-//		Debug.Log(request2);
-//		client.Send(request2, response2 => {
-//			Debug.Log(response2.ToString(true));
-//		});
 	}
 }
 
 public class TestClass
 {
 	public int level;
+	public string stat;
 }
