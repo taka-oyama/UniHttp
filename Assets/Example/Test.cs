5b80c3b508c4f3d555fb8ccabe1c174d105b4970
Taka Oyama <taka.oyama@gmail.com>
2017-10-30 01:28:15 +0900

    easier testing

--- Assets/Example/Test.cs
+++ Assets/Example/Test.cs
@@ -10,30 +10,21 @@ public class Test : MonoBehaviour
 	void Awake()
 	{
 		var httpSettings = new HttpSettings();
-		httpSettings.useCache = false;
+//		httpSettings.useCache = false;
+//		httpSettings.proxy = new HttpProxy("localhost", 3128);
 		httpManager = HttpManager.Initalize(httpSettings);
 	}
 
-	void Start ()
+	public void Send ()
 	{
 		var uri0 = new Uri("http://localhost:3000/large_file");
 		var request0 = new HttpRequest(HttpMethod.GET, uri0);
 
 		httpManager.Send(request0, response => {
 		});
-//
-//		var uri = new Uri("http://localhost:3000/test");
-//		IHttpData payload = new HttpJsonData(new TestClass() { level = 10, stat = "!!#^(0-=" });
-//		var request = new HttpRequest(HttpMethod.GET, uri);
-//		httpManager.Send(request, response => {
-//			for(int i = 0; i < 10; i++) {
-//				var uri1 = new Uri("http://localhost:3000/test");
-//				var request1 = new HttpRequest(HttpMethod.GET, uri1);
-//				httpManager.Send(request1, r => {
-//				});
-//			}
-//		});
 
+		httpManager.Send(request0, response => {
+		});
 	}
 }
 
