c3fc687cbe2ece2e6d82ae2ffd63298cfa1cb21d
Taka Oyama <taka.oyama@gmail.com>
2018-01-21 20:53:17 +0900

    better way to handle cookies

--- Assets/UniHttp/Support/Request/RequestHandler.cs
+++ Assets/UniHttp/Support/Request/RequestHandler.cs
@@ -84,14 +84,16 @@ namespace UniHttp
 		{
 			Dictionary<string, string> cookies = new Dictionary<string, string>();
 
-			foreach(Cookie cookie in cookieJar.FindMatch(request.Uri)) {
-				cookies.Add(cookie.name, cookie.value);
+			if(request.Headers.Exist(HeaderField.Cookie)) {
+				string[] presets = request.Headers[HeaderField.Cookie].Split(new string[] { "=", "; " }, StringSplitOptions.None);
+				for(int i = 0; presets.Length > i; i += 2) {
+					cookies.Add(presets[i], presets[i + 1]);
+				}
 			}
 
-			if(request.Headers.Exist(HeaderField.Cookie)) {
-				string[] presets = request.Headers[HeaderField.Cookie].Split(new string[]{"=", "; "}, StringSplitOptions.None);
-				for(int i = 0; presets.Length > 0; i += 2) {
-					cookies.Add(presets[i], presets[i + 1]);
+			foreach(Cookie cookie in cookieJar.FindMatch(request.Uri)) {
+				if(!cookies.ContainsKey(cookie.name)) {
+					cookies.Add(cookie.name, cookie.value);
 				}
 			}
 
